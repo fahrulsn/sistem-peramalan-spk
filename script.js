78d02b7755b1bf87389f7da26b267da7884e6345
false-197 <kreatif2.fs@gmail.com>
2023-04-24 15:50:46 +0700

    sistem peramalan suhu

--- script.js
+++ script.js
@@ -2,48 +2,61 @@ function hitung() {
   const input1 = parseInt(document.getElementById("data1").value);
   const input2 = parseInt(document.getElementById("data2").value);
   const input3 = parseInt(document.getElementById("data3").value);
+  const input4 = parseInt(document.getElementById("data4").value);
+  const input5 = parseInt(document.getElementById("data5").value);
+  const input6 = parseInt(document.getElementById("data6").value);
+  const input7 = parseInt(document.getElementById("data7").value);
   const period = parseInt(document.getElementById("period").value);
   const method = document.getElementById("method").value;
 
-  let data = [input1, input2, input3];
+  let data = [input1, input2, input3, input4, input5, input6, input7];
   let hasil = 0;
 
-  const sma = (data) => {
-    let sum = data.reduce((a, b) => a + b, 0);
-    return sum / data.length;
+  const sma = (data, period) => {
+    let newData = data.slice(data.length - period, data.length);
+    let sum = newData.reduce((a, b) => a + b, 0);
+    console.log(newData);
+    return sum / newData.length;
   };
 
-  const wma = (data) => {
+  const wma = (data, period) => {
+    let newData = data.slice(data.length - period, data.length);
     let sum = 0;
     let totalBobot = 0;
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < newData.length; i++) {
       let bobot = i + 1;
       totalBobot += bobot;
-      sum += data[i] * bobot;
+      sum += newData[i] * bobot;
     }
+    console.log(newData);
     return sum / totalBobot;
   };
 
   const xma = (data, period) => {
-    let newData = data.slice(0, period);
+    let firstxma = data.slice(0, period);
+    let newData = data.slice(period, data.length);
     let prevxma = 0;
     let newxma;
-    for (let i = period; i < data.length; i++) {
+    if (period == data.length) {
+      return sma(data, period);
+    }
+    for (let element of newData) {
       if (prevxma == 0) {
-        prevxma = sma(newData);
+        prevxma = sma(firstxma, firstxma.length);
       }
-      newxma = (2 / (period + 1)) * (data[i] - prevxma) + prevxma;
+      newxma = (2 / (period + 1)) * (element - prevxma) + prevxma;
       prevxma = newxma;
     }
+    console.log(newData, firstxma);
     return newxma;
   };
 
   switch (method) {
     case "SMA":
-      hasil = sma(data);
+      hasil = sma(data, period);
       break;
     case "WMA":
-      hasil = wma(data);
+      hasil = wma(data, period);
       break;
     case "XMA":
       hasil = xma(data, period);
